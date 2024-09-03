1a47aeb1b509b03870e8dc57e1de0e66ab7ea25d
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-02 21:59:16 -0400

    Create router and listing/error routes

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,6 +1,5 @@
 <?= loadPartial('head'); ?>
 <?= loadPartial('navbar'); ?>
-<?= loadPartial('showcase-search'); ?>
 <?= loadPartial('top-banner'); ?>
 
 <section>
@@ -168,10 +167,6 @@
                 </div>
             </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
 </section>
 
 <?= loadPartial('bottom-banner'); ?>
