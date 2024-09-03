06aee65b655c73c51072d940a8968bb7596264d0
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-02 20:55:55 -0400

    Create initial UI partials

--- helpers.php
+++ helpers.php
@@ -9,3 +9,35 @@
 function basePath($path = '') {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+    $viewPath = require basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View not found";
+    }
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name) {
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial not found";
+    }
+}
