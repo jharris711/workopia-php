a247958c333a9a01df962df44a595976a619d98e
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-03 22:12:41 -0400

    DB config setup

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 require '../helpers.php';
-require basePath('router.php');
+require basePath('Database.php');
+require basePath('Router.php');
+
+$config = require basePath('config/db.php');
+
+$db = new Database($config);
 
 $router = new Router();
 
