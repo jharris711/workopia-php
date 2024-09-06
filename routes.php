1b440eb372946cfe6be91d3ab9129fcc135417cd
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-06 00:08:26 -0400

    Home & Listing controller classes

--- routes.php
+++ routes.php
@@ -1,10 +1,9 @@
 <?php
 
-$router->get('/', 'HomeController@index');
 // Home
-/* $router->get('/', 'controllers/home.php');
+$router->get('/', 'HomeController@index');
 
 // Listings
-$router->get('/listings', 'controllers/listings/index.php');
-$router->get('/listings/create', 'controllers/listings/create.php');
-$router->get('/listing', 'controllers/listings/show.php'); */
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing', 'ListingController@show');
