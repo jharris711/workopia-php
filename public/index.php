2471c90110ce255b59da12d4a3205ed016876aee
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-05 23:03:04 -0400

    custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 require '../helpers.php';
-require basePath('Framework/Database.php');
-require basePath('Framework/Router.php');
+
+// Autoload classes from the Framework directory
+spl_autoload_register(function ($class) {
+    $path = basePath('Framework/') . $class . '.php';
+
+    if (file_exists($path)) {
+        require $path;
+    }
+});
 
 // Instantiate the router
 $router = new Router();
