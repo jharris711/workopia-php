2003b856b106f225a5a38bb480a7091e205d03e9
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-07 15:12:30 -0400

    Edit form display

--- routes.php
+++ routes.php
@@ -6,6 +6,9 @@ $router->get('/', 'HomeController@index');
 // Listings
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
+$router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
+
 $router->post('/listings', 'ListingController@store');
+$router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
